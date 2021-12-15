b08f86d025520ed8a98c8b21261052cab539571d
Karthik M <78743683+karthik0112@users.noreply.github.com>
2021-12-15 15:00:48 +0530

    Add files via upload

---
+++ error.js
@@ -0,0 +1,13 @@
+let value = 60;
+function start() {
+    function end() {
+
+    if(value > 50) {
+        console.log("function start");
+    }else {
+        console.log("Function Ended");
+    }
+    end();
+}
+}
+start();  //function call
---
+++ strict.js
@@ -0,0 +1,7 @@
+function strict() {
+    // Function-level strict mode syntax
+    'use strict';
+    function nested() { return 'And so am I!'; }
+    return "Hi!  I'm a strict mode function!  " + nested();
+  }
+  function notStrict() { return "I'm not strict."; }
